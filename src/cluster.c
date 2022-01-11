23b3fa36380dd8c531c6bef24b3337e9ed873621
Leo Martins <Leonardo.de-Oliveira-Martins@quadram.ac.uk>
2022-01-11 00:23:10 +0000

    cqueue struct

--- src/cluster.c
+++ src/cluster.c
@@ -2,6 +2,8 @@
 
 #include "fastaseq.h"
 
+typedef struct cqueue_struct* cqueue_t;
+
 typedef struct
 {
   struct arg_lit  *help;
@@ -17,9 +19,23 @@ typedef struct
   void **argtable;
 } arg_parameters;
 
+struct cqueue_struct
+{
+  int n_clust;
+  size_t *nchars;
+  char **seq, **name;
+  cluster_t *clust;
+};
+
 arg_parameters get_parameters_from_argv (int argc, char **argv);
 void del_arg_parameters (arg_parameters params);
 void print_usage (arg_parameters params, char *progname);
+
+cqueue_t new_cqueue (int n_clust, char *refseq, size_t nsites, int dist, int trim, int snps);
+char* get_outfile_prefix (const char *prefix, size_t *length);
+char* read_reference_sequence (readfasta_t *rfas, const char *filename, int nseqs);
+cqueue_t new_cqueue (int n_clust, char *refseq, size_t nsites, int dist, int trim, int snps);
+void del_cqueue (cqueue_t cq);
 
 arg_parameters
 get_parameters_from_argv (int argc, char **argv)
@@ -104,10 +120,10 @@ main (int argc, char **argv)
   bool end_of_file = false;
   int64_t time0[2], time1[2];
   double elapsed = 0.;
-  size_t trim = 0, outlength = 0, *nchars_vec;
-  char *outfilename = NULL, *refseq = NULL, **seq_vec, **name_vec;
+  size_t outlength = 0;
+  char *outfilename = NULL, *refseq = NULL;
   readfasta_t rfas;
-  cluster_t *clust;
+  cqueue_t cq;
 
   biomcmc_get_time (time0); 
   biomcmc_get_time (time1); 
@@ -127,56 +143,16 @@ main (int argc, char **argv)
   fflush(stderr);
 
   
-  if (params.out->count) {
-    outlength = strlen (params.out->filename[0]);
-    outfilename = (char*) biomcmc_malloc (outlength + 8); // suffixes added later
-    strncpy (outfilename, params.out->filename[0], outlength);
-    outfilename[outlength] = '\0';
-  }
-  else {
-    outlength = strlen ("cluster_uvaia");
-    outfilename = (char*) biomcmc_malloc (outlength + 8); // suffixes added later
-    strncpy (outfilename, "cluster_uvaia", outlength);
-    outfilename[outlength] = '\0';
-  }
-  strcpy (outfilename + outlength, ".csv.xz"); // CSV is saved several times
+  if (params.out->count) outfilename = get_outfile_prefix (params.out->filename[0], &outlength);
+  else                   outfilename = get_outfile_prefix ("cluster_uvaia", &outlength);
 
   /* 1. read reference sequence. If missing, use database to create a dummy sequence (composed of ACGT) */
-  if (params.ref->count) {
-    rfas = new_readfasta (params.ref->filename[0]);
-    readfasta_next (rfas);
-    refseq = rfas->seq;
-    rfas->seq = NULL;
-    fprintf (stderr, "Reading first sequence from %s as reference:\n  %s\n", params.ref->filename[0], rfas->name); 
-  }
-  else {
-    rfas = new_readfasta (params.fasta->filename[0]);
-    j = -1; count = 0xff; // count is number of non-ACGT sites
-    for (i = 0; (readfasta_next (rfas) > 0) && (count) && (i < 512); i++) {
-      if (j < 0) j = (int) rfas->seqlength;
-      else if (j != (int) rfas->seqlength) 
-        biomcmc_error ("Sequences should be aligned; first sequence has %d sites but sequence %s has %lu sites\n", j, rfas->name, rfas->seqlength);
-      count = accumulate_reference_sequence (&refseq, rfas->seq, rfas->seqlength); // merging of ACGT sites
-    }
-    fprintf (stderr, "Generating a reference from first 512 sequences or less in %s\n", params.fasta->filename[0]);
-  }
-  count = replace_Ns_from_reference (refseq, rfas->seqlength);
-  if (count) fprintf (stderr, "Reference still had %d Ns or indels which were replaced arbitrarily (it only makes program a bit slower).", count);
+  if (params.ref->count) refseq = read_reference_sequence (&rfas, params.ref->filename[0], 1);
+  else                   refseq = read_reference_sequence (&rfas, params.fasta->filename[0], 1024);
   
-  if (params.trim->ival[0] > 0) trim = (size_t) params.trim->ival[0];
-  if (trim > rfas->seqlength / 2.1) trim = rfas->seqlength / 2.1; // if we trim more than 1/2 the genome there's nothing left
-  if (params.dist->ival[0] > (int)(rfas->seqlength) / 10) params.dist->ival[0] = rfas->seqlength / 10;
 
   /* 1.1 one cluster per thread, with char pointers etc*/
-  clust = (cluster_t*) biomcmc_malloc (n_clust * sizeof (cluster_t));
-  for (c = 0; c < n_clust; c++) clust[c] = new_cluster (refseq, rfas->seqlength, params.dist->ival[0], trim, params.snps->ival[0]);
-  seq_vec    = (char**) biomcmc_malloc (n_clust *sizeof (char*)); // only pointer
-  name_vec   = (char**) biomcmc_malloc (n_clust *sizeof (char*)); // only pointer
-  nchars_vec = (size_t*) biomcmc_malloc (n_clust *sizeof (size_t)); // actual number
-  for (c = 0; c < n_clust; c++) { 
-    seq_vec[c] = name_vec[c] = NULL; 
-    nchars_vec[c] = 0;
-  }
+  cq = new_cqueue (n_clust, refseq, rfas->seqlength, params.dist->ival[0], params.trim->ival[0], params.snps->ival[0]);
  
   if (refseq) free (refseq);
   del_readfasta (rfas);
@@ -192,21 +168,21 @@ main (int argc, char **argv)
       for (c = 0; c < n_clust; c++) { // reads n_clust sequences
         if (readfasta_next (rfas) >= 0) {
           count++;
-          seq_vec[c] = rfas->seq; rfas->seq = NULL;
-          name_vec[c] = rfas->name; rfas->name = NULL;
-          nchars_vec[c] = rfas->seqlength;
+          cq->seq[c] = rfas->seq; rfas->seq = NULL;
+          cq->name[c] = rfas->name; rfas->name = NULL;
+          cq->nchars[c] = rfas->seqlength;
         }
         else {
-          seq_vec[c] = name_vec[c] = NULL;
+          cq->seq[c] = cq->name[c] = NULL;
           end_of_file = true;
         }
       } // single thread for() loop
 
-#pragma omp parallel for shared(c, count, clust, seq_vec, name_vec, nchars_vec)
-      for (c = 0; c < n_clust; c++) if (seq_vec[c]) {
-        check_seq_against_cluster (clust[c], &(seq_vec[c]), &(name_vec[c]), nchars_vec[c]);
-        if (!(clust[c]->n_fs % print_interval) && !(count%4)) { // cont%4 to avoid consecutive prints
-          fprintf (stderr, "Queue %3d / %d has %d clusters; %d sequences analysed in total;\n", c, n_clust, clust[c]->n_fs, count); 
+#pragma omp parallel for shared(c, count, cq)
+      for (c = 0; c < cq->n_clust; c++) if (cq->seq[c]) {
+        check_seq_against_cluster (cq->clust[c], &(cq->seq[c]), &(cq->name[c]), cq->nchars[c]);
+        if (!(cq->clust[c]->n_fs % print_interval) && !(count%4)) { // cont%4 to avoid consecutive prints
+          fprintf (stderr, "Queue %3d / %d has %d clusters; %d sequences analysed in total;\n", c, cq->n_clust, cq->clust[c]->n_fs, count); 
           fflush(stderr);
         }
       }
@@ -218,7 +194,7 @@ main (int argc, char **argv)
       }
       if ((count >= save_interval) && ((count % save_interval) < n_clust) && (elapsed > 30)) {
         fprintf (stderr, "Saving partial clustering info from %d sequences to file %s\n", count, outfilename); fflush(stderr);
-        save_neighbours_to_xz_file (clust, n_clust, outfilename);
+        save_neighbours_to_xz_file (cq->clust, cq->n_clust, outfilename);
       }
     } // while not end of file
 
@@ -227,7 +203,7 @@ main (int argc, char **argv)
   }  // for fasta file
 
   /* 2.1 create vector of SNP locations wrt reference */
-  generate_idx_from_cluster_list (clust, n_clust, 0);
+  generate_idx_from_cluster_list (cq->clust, cq->n_clust, 0);
 
 //#pragma omp parallel for shared(c, clust, j) private (count)
 //  for (c = 0; c < n_clust; c++) {
@@ -238,35 +214,99 @@ main (int argc, char **argv)
   /* 3. merge clusters */
 
   elapsed = biomcmc_update_elapsed_time (time1); // time1 is more finegrained than time0 
-  for (c = n_clust; c > 1; c = (c/2 + c%2)) { // reduce (outside parallel loop)
-#pragma omp parallel for shared(clust, c, j) private(count,i)
+  for (c = cq->n_clust; c > 1; c = (c/2 + c%2)) { // reduce (outside parallel loop)
+#pragma omp parallel for shared(cq, c, j) private(count,i)
     for (j = 0; j < c/2; j++) {
       i = j + c/2 + c%2;
-      fprintf (stderr, "Merging queues %d and %d out of %d (%d and %d nonredundant sequences)\n",j,i,c,clust[j]->n_fs, clust[i]->n_fs); fflush(stderr);
-      count = merge_clusters (clust[j], clust[i]); 
+      fprintf (stderr, "Merging queues %d and %d out of %d (%d and %d nonredundant sequences)\n",j,i,c,cq->clust[j]->n_fs, cq->clust[i]->n_fs); fflush(stderr);
+      count = merge_clusters (cq->clust[j], cq->clust[i]); 
       fprintf (stderr, "%d clusters coalesced between queues %d and %d\n", count, j, i); fflush(stderr);
     }
 
     fprintf (stderr, "%.3lf secs elapsed. Saving partial clustering info from %d sequences to file %s\n", biomcmc_update_elapsed_time (time1), count, outfilename); fflush(stderr);
-    save_neighbours_to_xz_file (clust, n_clust, outfilename);
+    save_neighbours_to_xz_file (cq->clust, cq->n_clust, outfilename);
   }
 
   //for (c = 0; c < n_clust; c++) qsort (clust[c]->fs, clust[c]->n_fs, sizeof (fastaseq_t), compare_fastaseq);
-  qsort (clust[0]->fs, clust[0]->n_fs, sizeof (fastaseq_t), compare_fastaseq);
+  qsort (cq->clust[0]->fs, cq->clust[0]->n_fs, sizeof (fastaseq_t), compare_fastaseq);
 
-  save_neighbours_to_xz_file (clust, 1, outfilename);
+  save_neighbours_to_xz_file (cq->clust, 1, outfilename);
   strcpy (outfilename + outlength, ".aln.xz");
-  save_cluster_to_xz_file (clust, 1, outfilename);
+  save_cluster_to_xz_file (cq->clust, 1, outfilename);
 
   fprintf (stderr, "Finished sorting clusters and saving files in %lf secs\n", biomcmc_update_elapsed_time (time0)); fflush(stderr);
 
   /* everybody is free to feel good */
   del_arg_parameters (params);
-  for (c = n_clust -1; c >= 0; c--) del_cluster (clust[c]); 
-  if (clust) free (clust);
-  if (seq_vec) free (seq_vec);
-  if (name_vec) free (name_vec);
-  if (nchars_vec) free (nchars_vec);
+  del_cqueue (cq);
   if (outfilename) free (outfilename);
   return EXIT_SUCCESS;
+}
+
+char*
+get_outfile_prefix (const char *prefix, size_t *length)
+{
+  char *filename = NULL;
+  *length = strlen (prefix);
+  filename = (char*) biomcmc_malloc (*length + 8); // suffixes added later
+  strncpy (filename, prefix, *length);
+  filename[*length] = '\0';
+  strcpy (filename + *length, ".csv.xz"); // CSV is saved several times
+  return filename;
+}
+  
+char*
+read_reference_sequence (readfasta_t *rfas, const char *filename, int nseqs)
+{
+  char *refseq = NULL;
+  int j = -1, i, count = 0xff; // count is number of non-ACGT sites
+  *rfas = new_readfasta (filename);
+  
+  fprintf (stderr, "Generating a reference from up to %d sequences in %s\n", nseqs, filename);
+  for (i = 0; (readfasta_next (*rfas) > 0) && (count) && (i < nseqs); i++) {
+    if (j < 0) j = (int) (*rfas)->seqlength;
+    else if (j != (int) (*rfas)->seqlength) biomcmc_error ("Unaligned sequences: first seq has %d sites but %s has %lu sites\n", j, (*rfas)->name, (*rfas)->seqlength);
+    count = accumulate_reference_sequence (&refseq, (*rfas)->seq, (*rfas)->seqlength); // merging of ACGT sites
+  }
+  count = replace_Ns_from_reference (refseq, (*rfas)->seqlength);
+  if (count) fprintf (stderr, "Reference still had %d Ns or indels which were replaced arbitrarily (it only makes program a bit slower).", count);
+  return refseq;
+
+}
+
+cqueue_t
+new_cqueue (int n_clust, char *refseq, size_t nsites, int dist, int trim, int snps)
+{
+  cqueue_t cq = (cqueue_t) biomcmc_malloc (sizeof (struct cqueue_struct));
+  int c;
+  cq->n_clust = n_clust;
+  cq->clust = (cluster_t*) biomcmc_malloc (n_clust * sizeof (cluster_t));
+
+  if (trim < 0) trim = 0;
+  if (trim > nsites / 2.1) trim = nsites / 2.1; // if we trim more than 1/2 the genome there's nothing left
+  if (dist > (int)(nsites) / 10) dist = (int) nsites / 10;
+
+  for (c = 0; c < n_clust; c++) cq->clust[c] = new_cluster (refseq, nsites, dist, (size_t) trim, snps);
+  cq->seq    = (char**) biomcmc_malloc (n_clust *sizeof (char*)); // only pointer
+  cq->name   = (char**) biomcmc_malloc (n_clust *sizeof (char*)); // only pointer
+  cq->nchars = (size_t*) biomcmc_malloc (n_clust *sizeof (size_t)); // actual number
+  for (c = 0; c < n_clust; c++) { 
+    cq->seq[c] = cq->name[c] = NULL; 
+    cq->nchars[c] = 0;
+  }
+  return cq;
+}
+
+void
+del_cqueue (cqueue_t cq)
+{
+  if (!cq) return;
+  int c;
+  for (c = cq->n_clust -1; c >= 0; c--) del_cluster (cq->clust[c]); 
+  if (cq->clust) free (cq->clust);
+  if (cq->seq) free (cq->seq);
+  if (cq->name) free (cq->name);
+  if (cq->nchars) free (cq->nchars);
+  free (cq);
+  return;
 }
